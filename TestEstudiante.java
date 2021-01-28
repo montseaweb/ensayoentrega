76bcbbb8d49850d722af0271961d8d8d1d5ae907
montseaweb <montseaweb@gmail.com>
2021-01-28 17:37:15 +0100

    Modificado desde aweb

--- TestEstudiante.java
+++ TestEstudiante.java
@@ -16,6 +16,8 @@ public class TestEstudiante
         e1.printEstudiante();
         Estudiante e2 = new Estudiante("Ana", 8);
         e2.printEstudiante();
+		Estudiante e3 = new Estudiante("Aweb", 9);
+        e3.printEstudiante();
 
     }
 }
